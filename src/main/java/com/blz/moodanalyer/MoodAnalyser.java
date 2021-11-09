a04b415b19e135a1a6da5ec14bd84ec34baed58a
Saurabh Siddharth <saurabhsiddharth68>
2021-11-09 23:28:30 +0530

    Given Null Mood

--- src/main/java/com/blz/moodanalyer/MoodAnalyser.java
+++ src/main/java/com/blz/moodanalyer/MoodAnalyser.java
@@ -1,15 +1,36 @@
 package com.blz.moodanalyer;
 
 public class MoodAnalyser {
-    public String analyserMood(String  message){
-        if(message.toLowerCase().contains("sad")){
-            return "SAD";
-        }
-        else if(message.toLowerCase().contains("any")){
-            return "HAPPY";
-        }
-        else{
-            return null;
+    private String message;
+
+    public MoodAnalyser(String message) throws MoodAnalyserException {
+        this.message = message;
+    }
+
+    public MoodAnalyser() {
+
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood() throws MoodAnalyserException {
+        try {
+            if (message == "")
+                throw new MoodAnalyserException("Entered Invalid Mood",
+                        MoodAnalyserException.ExceptionType.ENTERED_EMPTY);
+            if (message.toLowerCase().contains("sad")) {
+                return "SAD";
+            } else
+                return "HAPPY";
+        } catch (NullPointerException e) {
+            throw new MoodAnalyserException("Entered Invalid Mood",
+                    MoodAnalyserException.ExceptionType.ENTERED_NULL);
         }
     }
 }
---
+++ src/main/java/com/blz/moodanalyer/MoodAnalyserException.java
@@ -0,0 +1,15 @@
+package com.blz.moodanalyer;
+
+public class MoodAnalyserException extends Exception{
+    public String message;
+    public ExceptionType exceptionType;
+
+    enum ExceptionType {
+        ENTERED_NULL,ENTERED_EMPTY;
+    }
+
+    public MoodAnalyserException(String message, ExceptionType type) {
+        this.message = message;
+        this.exceptionType = type;
+    }
+}
