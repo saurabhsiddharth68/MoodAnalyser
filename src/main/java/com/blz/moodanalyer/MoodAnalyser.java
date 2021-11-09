ca3990f3b9f45704b73dae3cecaab754f022d39a
Saurabh Siddharth <saurabhsiddharth68>
2021-11-09 23:48:12 +0530

    Analyser Mood Happy or Sad

--- src/main/java/com/blz/moodanalyer/MoodAnalyser.java
+++ src/main/java/com/blz/moodanalyer/MoodAnalyser.java
@@ -1,36 +1,21 @@
 package com.blz.moodanalyer;
 
 public class MoodAnalyser {
-    private String message;
-
-    public MoodAnalyser(String message) throws MoodAnalyserException {
-        this.message = message;
-    }
-
-    public MoodAnalyser() {
-
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String analyseMood() throws MoodAnalyserException {
-        try {
-            if (message == "")
-                throw new MoodAnalyserException("Entered Invalid Mood",
-                        MoodAnalyserException.ExceptionType.ENTERED_EMPTY);
-            if (message.toLowerCase().contains("sad")) {
-                return "SAD";
-            } else
-                return "HAPPY";
-        } catch (NullPointerException e) {
-            throw new MoodAnalyserException("Entered Invalid Mood",
-                    MoodAnalyserException.ExceptionType.ENTERED_NULL);
+    public String analyseMood(String message) {
+        if (message.toLowerCase().contains("happy")) {
+            return "Happy";
+        } else if (message.toLowerCase().contains("sad")) {
+            return "Sad";
+        } else {
+            return null;
         }
+    }
+
+    public static void main(String[] args) {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am Happy");
+        System.out.println(mood);
+        mood = moodAnalyser.analyseMood("I am Sad");
+        System.out.println(mood);
     }
 }
