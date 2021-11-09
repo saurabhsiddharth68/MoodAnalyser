22852163c4acb7460e30f90862d54341be420ad3
Saurabh Siddharth <saurabhsiddharth68>
2021-11-09 23:15:51 +0530

    Test the Happy Mood

--- src/main/java/com/blz/moodanalyer/MoodAnalyser.java
+++ src/main/java/com/blz/moodanalyer/MoodAnalyser.java
@@ -5,6 +5,9 @@ public class MoodAnalyser {
         if(message.toLowerCase().contains("sad")){
             return "SAD";
         }
+        else if(message.toLowerCase().contains("any")){
+            return "HAPPY";
+        }
         else{
             return null;
         }
