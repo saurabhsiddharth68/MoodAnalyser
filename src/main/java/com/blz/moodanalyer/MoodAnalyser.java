c405516901f3302b59f357d4fbe243e3f3364d34
Saurabh Siddharth <saurabhsiddharth68>
2021-11-09 22:05:33 +0530

    Contructor for Sad

---
+++ src/main/java/com/blz/moodanalyer/MoodAnalyser.java
@@ -0,0 +1,32 @@
+package com.blz.moodanalyer;
+
+public class MoodAnalyser {
+    private String message;
+
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    public MoodAnalyser() {
+
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String analyseMood(String message) {
+
+        if (message.toLowerCase().contains("sad")) {
+            return "SAD";
+        } else if (message.toLowerCase().contains("any")) {
+            return "HAPPY";
+        } else {
+            return null;
+        }
+    }
+}
